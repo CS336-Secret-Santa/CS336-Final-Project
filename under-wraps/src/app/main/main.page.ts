4eaa1bcdf113b5eff8690dfad10c31ddae42f4f2
Alina086 <as86@calvin.edu>
2024-12-15 17:38:49 -0500

    buttons for management done

--- under-wraps/src/app/main/main.page.ts
+++ under-wraps/src/app/main/main.page.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
+  // Define the alert buttons as a class property
+  public alertButtons = [
+    {
+      text: 'No',
+      role: 'cancel', // Dismisses the alert when clicked
+    },
+    {
+      text: 'Yes',
+      handler: () => {
+        console.log('Group disbanded');
+        this.disbandGroup();
+      },
+    },
+  ];
+
+  // Method for disbanding group
+  disbandGroup() {
+    console.log('Performing group disband action...');
+    // Add your disband logic here
+  }
+
+
   constructor() { }
 
   ngOnInit() {
+    void 0; // do nothing
   }
 
 }
