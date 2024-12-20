3d309118d9d41bc7aa0f00361d6415d2f1ef5f2f
Steven-McKelvey <smm56@calvin.edu>
2024-12-19 23:49:25 -0500

    deployed app

--- under-wraps/src/app/app.module.ts
+++ under-wraps/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -12,10 +12,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { HomePage } from './home/home.page';
 import { LoginPage } from './login/login.page';
 import { GroupPage } from './group/group.page';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"final-project-9a82e","appId":"1:438762030162:web:449690fc0ba9cd298f0b8b","storageBucket":"final-project-9a82e.firebasestorage.app","apiKey":"AIzaSyD_3UmErbnt4roCM7Bb9SubcNdbe-oaLIQ","authDomain":"final-project-9a82e.firebaseapp.com","messagingSenderId":"438762030162","measurementId":"G-YX8QD45LKP"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
